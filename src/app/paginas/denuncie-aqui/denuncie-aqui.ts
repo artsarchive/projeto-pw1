b33f8f1189f2e49f7d4e22b6e6be95d43184ca5f
yohannd1 <mitonanan12@gmail.com>
2026-05-09 16:37:24 -0300

    exploda mapa

--- src/app/paginas/denuncie-aqui/denuncie-aqui.ts
+++ src/app/paginas/denuncie-aqui/denuncie-aqui.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { LeafletDirective } from '@bluehalo/ngx-leaflet';
+import { MapOptions, tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-denuncie-aqui',
-  imports: [],
+  imports: [LeafletDirective],
   templateUrl: './denuncie-aqui.html',
   styleUrl: './denuncie-aqui.css',
 })
-export class DenuncieAqui {}
+export class DenuncieAqui {
+  protected readonly mapOptions: MapOptions = {
+    layers: [
+      tileLayer('https://www.openstreetmap.org/search?lat=-13.86059&lon=-40.07263&zoom=15#map=15/-13.86060/-40.07261', { maxZoom: 18, attribution: '...' }),
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909),
+  };
+
+  ngOnInit() {}
+}
