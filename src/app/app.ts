490e27eaf9e3ac1cf0a33f5b9bddbe178805e7aa
Stella da Silva Ribas <stelladasilvaribas@gmail.com>
2026-05-09 15:26:17 -0300

    d3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Menu } from './a/menu/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
