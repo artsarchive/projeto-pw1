71d1dc2508c26d3ee417bf88a57ae9aec0445ffd
yohannd1 <mitonanan12@gmail.com>
2026-05-11 21:58:40 -0300

    oi meu nome é stella

--- src/app/paginas/inicio/inicio.ts
+++ src/app/paginas/inicio/inicio.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Menu } from '../../a/menu/menu';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink, Menu],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
