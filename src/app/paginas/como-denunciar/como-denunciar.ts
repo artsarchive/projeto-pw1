6db956ee30852de1005f9d306c33aeb6bb08e5b9
artsarchive <myartdonovan@gmail.com>
2026-05-11 22:26:52 -0300

    nova versao da pagina em andamento

--- src/app/paginas/como-denunciar/como-denunciar.ts
+++ src/app/paginas/como-denunciar/como-denunciar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Menu } from '../../a/menu/menu';
 
 @Component({
   selector: 'app-como-denunciar',
-  imports: [],
+  imports: [RouterLink, Menu],
   templateUrl: './como-denunciar.html',
   styleUrl: './como-denunciar.css',
 })
