b34af6c02b62f2cd558d08ddb6837eaa10b85628
yohannd1 <mitonanan12@gmail.com>
2026-05-18 18:01:31 -0300

    atualização de forms pt.1

--- src/app/paginas/denuncie-aqui/denuncie-aqui.ts
+++ src/app/paginas/denuncie-aqui/denuncie-aqui.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { LeafletDirective } from '@bluehalo/ngx-leaflet';
 import { MapOptions, tileLayer, latLng } from 'leaflet';
 import { Menu } from '../../a/menu/menu';
@@ -10,7 +12,7 @@ import { Menu } from '../../a/menu/menu';
     <div style="height: 100%" leaflet [leafletOptions]="mapOptions"></div>
   `,
 })
-export class Map {
+class Map {
   protected readonly mapOptions: MapOptions = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -21,10 +23,47 @@ export class Map {
   };
 }
 
+// TODO: https://angular.dev/guide/forms/template-driven-forms
+export class Form {
+  constructor(
+    public tipoViolencia: string,
+    public dataOcorrencia: string,
+    public horarioOcorrencia: string,
+    public pontoReferencia: string,
+    public local: string, // TODO: descobrir tipo
+  ) {}
+}
+
 @Component({
   selector: 'app-denuncie-aqui',
-  imports: [Map, Menu],
+  imports: [Map, Menu, FormsModule, KeyValuePipe],
   templateUrl: './denuncie-aqui.html',
   styleUrl: './denuncie-aqui.css',
 })
-export class DenuncieAqui {}
+export class DenuncieAqui {
+  readonly tiposViolencia = {
+    "trafico": "Tráfico",
+    "sexual": "Sexual",
+    "feminicidio": "Feminicídio",
+    "agressao": "Agressão",
+    "psicologica": "Psicológica",
+    "exploração": "Exploração",
+    "machismo": "Machismo",
+    "assalto": "Assalto",
+
+    // FIXME: esses aqui fazem sentido? eles tem overlap
+    "insulto": "Insulto",
+    "desrespeito": "Desrespeito",
+    "injustica": "Injustiça",
+    "indiferença": "Indiferença",
+  };
+
+  readonly horariosOcorrencia = {
+    "madrugada": "Madrugada (00:00 - 04:59)",
+    "manha": "Manhã (05:00 - 12:59)",
+    "tarde": "Tarde (13:00 - 17:59)",
+    "noite": "Noite (18:00 - 23:59)",
+  };
+
+  model = new Form("agressao", "", "tarde", "", "");
+}
