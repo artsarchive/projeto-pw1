7393a5c382b8c685fc4a8e2759437379eb2cb468
Stella da Silva Ribas <stelladasilvaribas@gmail.com>
2026-06-01 23:12:52 -0300

    Alteração do menu + página inicial

--- src/app/a/menu/menu.ts
+++ src/app/a/menu/menu.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+type MenuEntry = {
+  link: string;
+  name: string;
+};
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {}
+export class Menu {
+  protected readonly items: MenuEntry[] = [
+    { link: "/", name: "INÍCIO" },
+    { link: "/como-denunciar", name: "COMO DENUNCIAR" },
+    { link: "/denuncie-aqui", name: "DENUNCIE AQUI" },
+    { link: "/sobre-projeto", name: "SOBRE O PROJETO" },
+    { link: "/areas-afetadas", name: "ÁREAS AFETADAS" },
+  ];
+}
