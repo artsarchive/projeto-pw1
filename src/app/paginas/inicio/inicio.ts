d1b9daa2fde8c78ee7f3aea94119e7115a5ea53b
Stella da Silva Ribas <stelladasilvaribas@gmail.com>
2026-06-25 17:18:52 -0300

    adição do rodapé

--- src/app/paginas/inicio/inicio.ts
+++ src/app/paginas/inicio/inicio.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Menu } from '../../a/menu/menu';
 import { Feedback } from "../../a/feedback/feedback";
+import { Rodape } from '../../a/rodape/rodape';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink, Menu, Feedback],
+  imports: [RouterLink, Menu, Feedback, Rodape],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
